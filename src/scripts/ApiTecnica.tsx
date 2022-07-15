ac3c72ab36481808ae42d656e0e9340dc7bc0589
Sebastian Cerviño <cervisebas@gmail.com>
2022-07-15 17:01:31 -0300

    15/07/2022 17:01hs

--- src/scripts/ApiTecnica.tsx
+++ src/scripts/ApiTecnica.tsx
@@ -2,7 +2,8 @@ import moment from "moment";
 import { encode, decode } from "base-64";
 import StudentSystem from "./scripts/students";
 
-const UrlBase = "http://localhost/TecnicaDigitalApi";
+//const UrlBase = "http://localhost/TecnicaDigitalApi";
+const UrlBase = "https://tecnica-digital.ga/";
 const KeyAccess = encode("Zr4u7x!A%D*G-KaNdRgUkXp2s5v8y/B?E(H+MbQeShVmYq3t6w9z$C&F)J@NcRfU");
 const GetKeyVerify = ()=>{
     var keyAccess = decode("VUdMV011UDcwZTZjTm9MTFRIZjZwTjVPOEFDZFJZR3BlVjNJczJ2TXE5QmRoenFtYkk=");
---
+++ src/scripts/SyncData.tsx
@@ -0,0 +1,35 @@
+import moment from "moment";
+
+export default class SyncData {
+    constructor() {
+        this.startSync = this.startSync.bind(this);
+    }
+    private timeSync = [
+        { hour: 7, minMinutes: 31, maxMinutes: 46, result: '7:15' },
+        { hour: 8, minMinutes: 56, maxMinutes: 59, result: '8:40' },
+        { hour: 9, minMinutes: 0, maxMinutes: 11, result: '8:40' },
+        { hour: 9, minMinutes: 51, maxMinutes: 59, result: '9:50' },
+        { hour: 10, minMinutes: 0, maxMinutes: 6, result: '9:50' },
+        { hour: 11, minMinutes: 16, maxMinutes: 31, result: '11:00' },
+        { hour: 13, minMinutes: 31, maxMinutes: 46, result: '13:15' },
+        { hour: 14, minMinutes: 36, maxMinutes: 51, result: '14:25' },
+        { hour: 15, minMinutes: 46, maxMinutes: 59, result: '15:35' },
+        { hour: 16, minMinutes: 0, maxMinutes: 1, result: '15:35' },
+        { hour: 16, minMinutes: 56, maxMinutes: 59, result: '16:45' },
+        { hour: 17, minMinutes: 0, maxMinutes: 11, result: '16:45' }
+    ];
+    init() {
+        setInterval(this.startSync, 60000);
+    }
+    startSync() {
+        var now: { hour: number, minutes: number } = { hour: parseInt(moment().format('HH')), minutes: parseInt(moment().format('mm')) };
+        var find = this.timeSync.find((value)=>{
+            if (value.hour == now.hour) {
+                if (now.minutes >= value.minMinutes && now.minutes <= value.maxMinutes) return value;
+            }
+        });
+        if (find) {
+            
+        }
+    }
+}
